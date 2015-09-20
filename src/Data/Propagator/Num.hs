f04a6f57de7feaaa0e4abe47a4ab7dc11f509826
Edward Kmett <ekmett@gmail.com>
2015-09-20 00:34:12 -0400

    timesFractional, add Double/Floating PropagatedNum instances

--- src/Data/Propagator/Num.hs
+++ src/Data/Propagator/Num.hs
@@ -9,18 +9,20 @@ import Data.Propagator.Cell
 import Data.Propagator.Class
 import Numeric.Natural
 
-class (Propagated a, Num a) => PropagatedNum a where
+class Propagated a => PropagatedNum a where
   plus :: Cell s a -> Cell s a -> Cell s a -> ST s ()
+  default plus :: Num a => Cell s a -> Cell s a -> Cell s a -> ST s ()
   plus x y z = do
     lift2 (+) x y z
     lift2 (-) z x y
     lift2 (-) z y z
 
   times :: Cell s a -> Cell s a -> Cell s a -> ST s ()
+  default times :: Num a => Cell s a -> Cell s a -> Cell s a -> ST s ()
   times = lift2 (*)
 
   cabs :: Cell s a -> Cell s a -> ST s ()
-  default cabs :: Eq a => Cell s a -> Cell s a -> ST s ()
+  default cabs :: (Num a, Eq a) => Cell s a -> Cell s a -> ST s ()
   cabs x y = do
     lift1 abs x y
     watch y $ \b -> when (b == 0) $ write x 0
@@ -45,20 +47,29 @@ instance PropagatedNum Int
 instance PropagatedNum Word where
   cabs = unify
 
+timesFractional :: (Eq a, Fractional a) => Cell s a -> Cell s a -> Cell s a -> ST s ()
+timesFractional x y z = do
+  watch x $ \a ->
+    if a == 0
+    then write z 0
+    else do
+      with y $ \b -> write z (a*b)
+      with z $ \c -> write y (c/a) -- a /= 0 determined above
+  watch y $ \b ->
+    if b == 0
+    then write z 0
+    else do
+      with x $ \a -> write z (a*b)
+      with z $ \c -> write x (c/b) -- b /= 0 determined above
+  watch z $ \c -> do
+    with x $ \a -> when (a /= 0) $ write y (c/a)
+    with y $ \b -> when (b /= 0) $ write x (c/b)
+
 instance PropagatedNum Rational where
-  times x y z = do
-    watch x $ \a ->
-      if a == 0
-      then write z 0
-      else do
-        with y $ \b -> write z (a*b)
-        with z $ \c -> write y (c/a) -- a /= 0 determined above
-    watch y $ \b ->
-      if b == 0
-      then write z 0
-      else do
-        with x $ \a -> write z (a*b)
-        with z $ \c -> write x (c/b) -- b /= 0 determined above
-    watch z $ \c -> do
-      with x $ \a -> when (a /= 0) $ write y (c/a)
-      with y $ \b -> when (b /= 0) $ write x (c/b)
+  times = timesFractional
+
+instance PropagatedNum Double where
+  times = timesFractional
+
+instance PropagatedNum Float where
+  times = timesFractional
