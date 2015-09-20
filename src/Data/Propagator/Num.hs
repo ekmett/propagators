d05c8cb304969a5a2d232d4d76d9e91388203b66
Edward Kmett <ekmett@gmail.com>
2015-09-20 00:28:13 -0400

    PropagatedNum rational

--- src/Data/Propagator/Num.hs
+++ src/Data/Propagator/Num.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Data.Propagator.Num where
 
 import Control.Monad
@@ -10,13 +13,14 @@ class (Propagated a, Num a) => PropagatedNum a where
   plus :: Cell s a -> Cell s a -> Cell s a -> ST s ()
   plus x y z = do
     lift2 (+) x y z
-    lift2 (-) z x y 
+    lift2 (-) z x y
     lift2 (-) z y z
 
   times :: Cell s a -> Cell s a -> Cell s a -> ST s ()
   times = lift2 (*)
 
-  cabs :: Eq a => Cell s a -> Cell s a -> ST s ()
+  cabs :: Cell s a -> Cell s a -> ST s ()
+  default cabs :: Eq a => Cell s a -> Cell s a -> ST s ()
   cabs x y = do
     lift1 abs x y
     watch y $ \b -> when (b == 0) $ write x 0
@@ -36,7 +40,25 @@ instance PropagatedNum Natural where
       else watch y $ \ b -> when (b /= 0) $ write x 0
   cabs = unify
 
-instance PropagatedNum Int 
+instance PropagatedNum Int
+
 instance PropagatedNum Word where
   cabs = unify
 
+instance PropagatedNum Rational where
+  times x y z = do
+    watch x $ \a ->
+      if a == 0
+      then write z 0
+      else do
+        with y $ \b -> write z (a*b)
+        with z $ \c -> write y (c/a) -- a /= 0 determined above
+    watch y $ \b ->
+      if b == 0
+      then write z 0
+      else do
+        with x $ \a -> write z (a*b)
+        with z $ \c -> write x (c/b) -- b /= 0 determined above
+    watch z $ \c -> do
+      with x $ \a -> when (a /= 0) $ write y (c/a)
+      with y $ \b -> when (b /= 0) $ write x (c/b)
