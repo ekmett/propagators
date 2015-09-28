68ed001648f6d58bbced125bdf1267fad087e4a5
Edward Kmett <ekmett@gmail.com>
2015-09-27 23:16:11 -0400

    allow the creation of stable names (and use unique for the global unique supply)

--- src/Data/Propagator/Name.hs
+++ src/Data/Propagator/Name.hs
@@ -1,10 +1,11 @@
--- | Inspired by the names in nominal adapton.
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE MagicHash #-}
+-- | Inspired by the names in nominal adapton.
 module Data.Propagator.Name
   ( Name
   , fresh
+  , stable
   , fork
   , pair
   , child
@@ -12,47 +13,54 @@ module Data.Propagator.Name
   , height
   ) where
 
+import Control.Concurrent.Unique
 import Control.Monad.Primitive
 import Data.Bits
 import Data.Hashable
 import Data.String
 import GHC.Prim
-import GHC.Types
+import System.Mem.StableName
 
 data Name
-  = U {-# UNPACK #-} !Int (MutableByteArray# RealWorld)
+  = U {-# UNPACK #-} !Unique
+  | SN {-# UNPACK #-} !(StableName Any)
   | S {-# UNPACK #-} !Int String
   | P {-# UNPACK #-} !Int {-# UNPACK #-} !Int !Name !Name
   | C {-# UNPACK #-} !Int {-# UNPACK #-} !Int {-# UNPACK #-} !Int !Name
 
 instance Eq Name where
-  U _ i     == U _ j     = isTrue# (sameMutableByteArray# i j)
+  U i       == U j       = i == j
+  SN i      == SN j      = i == j
   S i s     == S j t     = i == j && s == t
   P i _ p q == P j _ r s = i == j && p == r && q == s
   C i _ m p == C j _ n q = i == j && m == n && p == q
   _ == _ = False
 
 instance Show Name where
-  showsPrec d (U i _)   = showParen (d > 10) $ showString "U " . showsPrec 11 i . showString " ..."
-  showsPrec d (S _ s)   = showsPrec d s
+  showsPrec _ (U i)       = showString "#" . showsPrec 11 (hash i)
+  showsPrec _ (SN i)      = showString "%" . showsPrec 11 (hashStableName i)
+  showsPrec d (S _ s)     = showsPrec d s
   showsPrec d (P i h l r) = showParen (d > 10) $ showString "P " . showsPrec 11 i . showChar ' ' . showsPrec 11 h . showChar ' ' . showsPrec 11 l . showChar ' ' . showsPrec 11 r
   showsPrec d (C i h n p) = showParen (d > 10) $ showString "C " . showsPrec 11 i . showChar ' ' . showsPrec 11 h . showChar ' ' . showsPrec 11 n . showChar ' ' . showsPrec 11 p
 
 -- | Has a negative binomial distribution. Same for any forked children.
 height :: Name -> Int
-height (U i _) = ffs i
+height (U i)   = ffs (hash i)
+height (SN i)  = ffs (hashStableName i)
 height (S i _) = ffs i
 height (P _ h _ _) = h
 height (C _ h _ _) = h
 {-# INLINE height #-}
 
 instance Hashable Name where
-  hashWithSalt d (U i _) = hashWithSalt d i
+  hashWithSalt d (U i) = hashWithSalt d i
+  hashWithSalt d (SN i) = hashWithSalt d i
   hashWithSalt d (S i _) = hashWithSalt d i
   hashWithSalt d (P i _ _ _) = hashWithSalt d i
   hashWithSalt d (C i _ _ _) = hashWithSalt d i
   {-# INLINE hashWithSalt #-}
-  hash (U i _) = i
+  hash (U i) = hash i
+  hash (SN i) = hashStableName i
   hash (S i _) = i
   hash (P i _ _ _) = i
   hash (C i _ _ _) = i
@@ -70,9 +78,13 @@ instance IsString Name where
 
 -- | Generate a fresh name.
 fresh :: PrimMonad m => m Name
-fresh = unsafePrimToPrim $ IO $ \s -> case newByteArray# 0# s of
-  (# s', ba #) -> (# s', U (I# (addr2Int# (unsafeCoerce# ba))) ba #)
+fresh = unsafePrimToPrim $ U <$> newUnique
 {-# INLINE fresh #-}
+
+-- | Obtain a stable name.
+stable :: PrimMonad m => a -> m Name
+stable a = unsafePrimToPrim $ SN <$> (unsafeCoerce# makeStableName a :: IO (StableName Any))
+{-# INLINE stable #-}
 
 -- | Obtain the name of two children deterministically.
 fork :: Name -> (Name, Name)
