882bb8433e3be82be4ae10ff61f69949e5cd4238
Edward Kmett <ekmett@gmail.com>
2015-10-18 22:16:56 -0400

    move IVar into Par so we can try out a "simple" IVar/LVar.

--- src/Model/Subscription.hs
+++
@@ -1,9 +0,0 @@
-module Model.Subscription where
-
-import Model.Par
-
-newtype Subscription = Subscription { cancel :: Par () }
-
-instance Monoid Subscription where
-  mempty = Subscription (return ())
-  mappend (Subscription m) (Subscription n) = Subscription (m >> n)
