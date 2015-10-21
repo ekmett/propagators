7ce268af67621e512105758b136526541be2fc78
Edward Kmett <ekmett@gmail.com>
2015-10-21 19:26:46 -0400

    mask to prevent us being killed while we are waking up blocked computations

--- src/Model/Par/IVar.hs
+++ src/Model/Par/IVar.hs
@@ -24,19 +24,19 @@ newIVar :: Par (IVar a)
 newIVar = io $ IVar <$> newIORef (Right [])
 
 readIVar :: IVar a -> Par a
-readIVar (IVar r) = Par $ \k -> join $ liftIO $ atomicModifyIORef' r $ \case
+readIVar (IVar r) = Par $ \k -> mask_ $ join $ liftIO $ atomicModifyIORef' r $ \case
   l@(Left a) -> (l, k a)
   Right ks   -> (Right (k:+ks), addKarma (-1) >> schedule)
 
 writeIVar :: Eq a => IVar a -> a -> Par ()
-writeIVar (IVar r) a = Par $ \k -> join $ liftIO $ atomicModifyIORef' r $ \case
+writeIVar (IVar r) a = Par $ \k -> mask_ $ join $ liftIO $ atomicModifyIORef' r $ \case
   l@(Left b)
     | a == b    -> (l, k ())
     | otherwise -> (l, liftIO $ throwIO Contradiction)
   Right ks      -> (Left a, do for_ ks (\k' -> defer $ k' a); addKarma (length ks); k () )
 
 unsafeWriteIVar :: IVar a -> a -> Par ()
-unsafeWriteIVar (IVar r) a = Par $ \k  -> join $ liftIO $ atomicModifyIORef' r $ \case
+unsafeWriteIVar (IVar r) a = Par $ \k  -> mask_ $ join $ liftIO $ atomicModifyIORef' r $ \case
   l@Left{} -> (l, k ())
   Right ks -> (Left a, do for_ ks (\k' -> defer $ k' a); addKarma (length ks); k () )
 
